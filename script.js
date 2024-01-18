69f2bbb034560a0abf11ee96bacb4f9547d8891b
abc <abc@example.com>
2024-01-18 12:23:37 +0530

    Add your commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const elGrid = document.querySelector(".grid");
+const elOrder = document.querySelector("#order");
+const order = [...elGrid.children].map(el => el.dataset.index);
+console.log(order)
+let elDrag;
+
+const showOrder = () => elOrder.textContent = `Order: ${order}`;
+
+const events = {
+  dragstart() { elDrag = this; },
+  dragover(ev) { ev.preventDefault(); },
+  drop() {
+    if (elDrag === this) return;
+    const ia = [...elGrid.children].indexOf(elDrag);
+    const ib = [...elGrid.children].indexOf(this);
+    elDrag.replaceWith(this.cloneNode(true));
+    this.replaceWith(elDrag);
+    [order[ia], order[ib]] = [order[ib], order[ia]];
+    showOrder();
+  }
+};
+
+[...elGrid.children].forEach((el, i) => el.draggable = true);
+["dragstart", "dragover", "drop"].forEach(evName => {
+  elGrid.addEventListener(evName, (ev) => {
+    const elItem = ev.target.closest(".item");
+    if (!elItem) return; 
+    events[evName].call(elItem, ev);
+  });
+});
+showOrder();
